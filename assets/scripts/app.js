bcc8d25bc4599936b9de6d92a229cbb3b74b41d8
Aman Anand <aman.anand@argildx.net>
2021-11-27 22:07:04 +0530

    added classes for ProductList and ProductItem

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,8 +6,29 @@ class Product{
         this.description = description;
     }
 }
-const productsList ={
-    products:[
+class ProductItem{
+    constructor(prod){
+        this.prod = prod;
+    }
+    render(){
+        const prodEl = document.createElement('li');
+        $(prodEl).addClass('product-item');
+        $(prodEl).html(`
+        <div>
+            <img src =${this.prod.imageUrl} alt=${this.prod.name}/>
+            <div class= "product-item__content">
+                <h2>${this.prod.name}</h2>
+                <h3>\$${this.prod.price}</h3>
+                <p>${this.prod.description}</p>
+                <button>Add to Cart</button>  
+            </div>
+                
+        </div>`);
+        return prodEl;     
+    }
+}
+class ProductsList{
+    products=[
         new Product(
             'Pillows',
             'https://th.bing.com/th/id/OIP.DnCGtHj_-3GzszUOoz680AHaHa?w=172&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7',
@@ -20,29 +41,17 @@ const productsList ={
             900,
             'Classy looking bed sheets'
         )
-    ],
-    render:function(){
+    ];
+    render(){
         const renderHook = $('#app');
         const prodList = document.createElement('ul');
         $(prodList).addClass('product-list');
         productsList.products.forEach(prod=>{
-            const prodEl = document.createElement('li');
-            $(prodEl).addClass('product-item');
-            $(prodEl).html(`
-            <div>
-                <img src =${prod.imageUrl} alt=${prod.name}/>
-                <div class= "product-item__content">
-                    <h2>${prod.name}</h2>
-                    <h3>\$${prod.price}</h3>
-                    <p>${prod.description}</p>
-                    <button>Add to Cart</button>  
-                </div>
-                 
-            </div>`);
-            $(prodList).append($(prodEl));
+            const productItem = new ProductItem(prod); 
+            $(prodList).append(productItem.render());
         });
-        console.log(prodList);
         renderHook.append(prodList);
     }
-};
+}
+const productsList = new ProductsList();
 productsList.render()
